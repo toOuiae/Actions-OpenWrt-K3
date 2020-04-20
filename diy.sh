4786d38c3919502281079737f0e0e01402cd5e37
Ouiae <76885266@qq.com>
2020-04-21 03:55:48 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,7 +7,7 @@
 #=================================================
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-src-git helloworld https://github.com/fw876/helloworld
-src-git rosy https://github.com/rosywrt/luci-theme-rosy.git
-src-git argon https://github.com/jerrykuku/luci-theme-argon.git
-src-git lienol https://github.com/Lienol/openwrt-package.git
+echo src-git helloworld https://github.com/fw876/helloworld >> feeds.conf.default
+echo src-git rosy https://github.com/rosywrt/luci-theme-rosy.git >> feeds.conf.default
+echo src-git argon https://github.com/jerrykuku/luci-theme-argon.git >> feeds.conf.default
+echo src-git lienol https://github.com/Lienol/openwrt-package.git >> feeds.conf.default
